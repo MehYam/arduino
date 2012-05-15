272538fe4d8d29ae45c8efa12d815025c86075df
Kai Arnold <cdn_kai@yahoo.com>
2012-05-14 18:44:27 -0700

    some initial hacked up samples

---
+++ AnalogReadSerial/AnalogReadSerial.ino
@@ -0,0 +1,25 @@
+/*
+  AnalogReadSerial
+ Reads an analog input on pin 0, prints the result to the serial monitor 
+ 
+ This example code is in the public domain.
+ */
+
+const int led = 13;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  const int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  
+  const int d = sensorValue/2 + 10;
+  delay(d);
+  digitalWrite(led, HIGH);
+  delay(d);
+  digitalWrite(led, LOW);
+}
---
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -0,0 +1,26 @@
+/*
+  DigitalReadSerial
+ Reads a digital input on pin 2, prints the result to the serial monitor 
+ 
+ This example code is in the public domain.
+ */
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT);
+}
+
+int lastVal = 0;
+void loop() {
+  int sensorValue = digitalRead(2);
+  if (sensorValue != lastVal)
+  {
+    Serial.print("Change to ");
+    Serial.println(sensorValue);
+    
+    lastVal = sensorValue;
+  }
+}
+
+
+
